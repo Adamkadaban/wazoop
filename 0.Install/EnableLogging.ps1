010b43685b7dcdd8187214b12f99266a21a5c7eb
Adamkadaban <adamkadaban@gmail.com>
2023-11-21 04:05:17 -0500

    added comments + adcs logging

--- 0.Install/EnableLogging.ps1
+++ 0.Install/EnableLogging.ps1
@@ -1,7 +1,15 @@
+#######################################################
+# Enable Logging to make sure we see things in sysmon #
+#######################################################
+
+#--------------------------------------#
+# Event ID 4719 is audit policy change #
+#--------------------------------------#
+
 # Enable Powershell Logging
-## Script Block Logging
 reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging /v EnableScriptBlockLogging /t REG_DWORD /d 1 /f
-## Transcription Logging
 reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\PowerShell\Transcription /v EnableTranscription /t REG_DWORD /d 1 /f
 
+# Enable ADCS Auditing
+auditpol /set /Category:"Object Access" /subcategory:"Certification Services" /success:enable /failure:enable
 
