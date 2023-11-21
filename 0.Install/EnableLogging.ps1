814ccb04091d2c3a63a3aefe40a9011412741d67
Adamkadaban <adamkadaban@gmail.com>
2023-11-21 04:11:37 -0500

    I was wrong. caps doesn't matter, but /Category does everything at once. Only subcategory matters here

--- 0.Install/EnableLogging.ps1
+++ 0.Install/EnableLogging.ps1
@@ -11,21 +11,21 @@ reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging /
 reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\PowerShell\Transcription /v EnableTranscription /t REG_DWORD /d 1 /f
 
 # Enable ADCS Auditing
-auditpol /set /Category:"Object Access" /Subcategory:"Certification Services" /success:enable /failure:enable
+auditpol /set /Subcategory:"Certification Services" /success:enable /failure:enable
 
 # Enable Process Execution Logging
-auditpol /set /Category:"Detailed Tracking" /Subcategory:"Process Creation" /success:enable /failure:enable
+auditpol /set /Subcategory:"Process Creation" /success:enable /failure:enable
 
 # Enable Account Logon/Logoff Logging
-auditpol /set /Category:"Logon/Logoff" /Subcategory:"Logon" /success:enable /failure:enable
-auditpol /set /Category:"Logon/Logoff" /Subcategory:"Logoff" /success:enable /failure:enable
+auditpol /set /Subcategory:"Logon" /success:enable /failure:enable
+auditpol /set /Subcategory:"Logoff" /success:enable /failure:enable
 
 # Enable File System Object Auditing
-auditpol /set /Category:"Object Access" /Subcategory:"File System" /success:enable /failure:enable
+auditpol /set /Subcategory:"File System" /success:enable /failure:enable
 
 # Enable Kerberos Logging
-auditpol /set /Category:"Account Logon" /Subcategory:"Kerberos Service Ticket Operations" /success:enable /failure:enable
-auditpol /set /Category:"Account Logon" /Subcategory:"Kerberos Authentication Service" /success:enable /failure:enable
+auditpol /set /Subcategory:"Kerberos Service Ticket Operations" /success:enable /failure:enable
+auditpol /set /Subcategory:"Kerberos Authentication Service" /success:enable /failure:enable
 
 # Enable Event Log Access Auditing
-auditpol /set /Category:"System" /Subcategory:"System Integrity" /success:enable /failure:enable
+auditpol /set /Subcategory:"System Integrity" /success:enable /failure:enable
