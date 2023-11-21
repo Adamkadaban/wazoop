89c5d0c0d55baf436c50fd4553ac90c413f397d8
Adamkadaban <adamkadaban@gmail.com>
2023-11-21 04:08:05 -0500

    subcategory -> Subcategory apparently caps matters

--- 0.Install/EnableLogging.ps1
+++ 0.Install/EnableLogging.ps1
@@ -11,21 +11,21 @@ reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging /
 reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\PowerShell\Transcription /v EnableTranscription /t REG_DWORD /d 1 /f
 
 # Enable ADCS Auditing
-auditpol /set /Category:"Object Access" /subcategory:"Certification Services" /success:enable /failure:enable
+auditpol /set /Category:"Object Access" /Subcategory:"Certification Services" /success:enable /failure:enable
 
 # Enable Process Execution Logging
-auditpol /set /Category:"Detailed Tracking" /subcategory:"Process Creation" /success:enable /failure:enable
+auditpol /set /Category:"Detailed Tracking" /Subcategory:"Process Creation" /success:enable /failure:enable
 
 # Enable Account Logon/Logoff Logging
-auditpol /set /Category:"Logon/Logoff" /subcategory:"Logon" /success:enable /failure:enable
-auditpol /set /Category:"Logon/Logoff" /subcategory:"Logoff" /success:enable /failure:enable
+auditpol /set /Category:"Logon/Logoff" /Subcategory:"Logon" /success:enable /failure:enable
+auditpol /set /Category:"Logon/Logoff" /Subcategory:"Logoff" /success:enable /failure:enable
 
 # Enable File System Object Auditing
-auditpol /set /Category:"Object Access" /subcategory:"File System" /success:enable /failure:enable
+auditpol /set /Category:"Object Access" /Subcategory:"File System" /success:enable /failure:enable
 
 # Enable Kerberos Logging
-auditpol /set /Category:"Account Logon" /subcategory:"Kerberos Service Ticket Operations" /success:enable /failure:enable
-auditpol /set /Category:"Account Logon" /subcategory:"Kerberos Authentication Service" /success:enable /failure:enable
+auditpol /set /Category:"Account Logon" /Subcategory:"Kerberos Service Ticket Operations" /success:enable /failure:enable
+auditpol /set /Category:"Account Logon" /Subcategory:"Kerberos Authentication Service" /success:enable /failure:enable
 
 # Enable Event Log Access Auditing
-auditpol /set /Category:"System" /subcategory:"System Integrity" /success:enable /failure:enable
+auditpol /set /Category:"System" /Subcategory:"System Integrity" /success:enable /failure:enable
